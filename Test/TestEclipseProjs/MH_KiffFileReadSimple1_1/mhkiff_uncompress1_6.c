567371da0ef7474fb9c62e919b8ee99af519418c
Rintaro Saito <rsaito@kazap.iab.keio.ac.jp>
2020-09-23 15:04:50 +0900

    ...

---
+++ Test/TestEclipseProjs/MH_KiffFileReadSimple1_1/mhkiff_uncompress1_6.c
@@ -0,0 +1,99 @@
+/*
+ * mhkiff_uncompress1_2.c
+ *
+ *  Created on: 2020/09/22
+ *      Author: rsaito
+ */
+
+/* On Mac,
+ * gcc -c mhkiff_uncompress1_6.c
+ * gcc -L/usr/lib mhkiff_uncompress1_6.o -o mhkiff_uncompress1_6 -lz
+ */
+
+#include <zlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void read_uncompress_kiffchunk(
+		char **, int *, int *, int *,
+		unsigned char *, int *);
+
+void read_uncompress_kiffchunk(
+		char **filename,
+		int *seekpos,
+		int *compressed_size,
+		int *uncompressed_size,
+		unsigned char *uncompressbuf,
+		int *res_status){
+
+	FILE *fp;
+	unsigned char *compressedbuf;
+	int uncompress_res;
+
+	if((fp = fopen(*filename, "r")) == NULL){
+		*res_status = -1001;
+		printf("Failed to open %s\n", *filename);
+		return;
+	} else {
+		printf("Successfully opened %s\n", *filename);	
+	}
+
+	printf("Seek position: %d  Compressed size: %d  Uncompressed size: %d\n",
+			*seekpos, *compressed_size, *uncompressed_size);
+
+
+	if(fseek(fp, *seekpos, SEEK_SET) != 0){
+		*res_status = -1002;
+		return;
+	}
+
+	if((compressedbuf = (unsigned char *)malloc(*compressed_size * sizeof(unsigned char))) == NULL){
+		*res_status = -1003;
+		return;
+	}
+
+	if(fread(compressedbuf, sizeof(char), *compressed_size, fp) < 0){
+		*res_status = -1004;
+		return;
+	}
+
+	uncompress_res = uncompress(
+			uncompressbuf, uncompressed_size,
+			compressedbuf, *compressed_size);
+	// https://refspecs.linuxbase.org/LSB_4.1.0/LSB-Core-generic/LSB-Core-generic/zlib-uncompress-1.html
+
+	printf("Z_OK: %d, Z_BUF_ERROR: %d, Z_MEM_ERROR: %d, Z_DATA_ERROR: %d\n",
+			Z_OK, Z_BUF_ERROR, Z_MEM_ERROR, Z_DATA_ERROR);
+	printf("Uncompress result: %d\n", uncompress_res);
+
+	free(compressedbuf);
+	// free(uncompressbuf);
+
+	*res_status = uncompress_res;
+
+}
+
+
+
+int main(){
+
+	int ret_val;
+	int seek_pos          = 19853425;
+	int compressed_size   = 1453424;
+	int uncompressed_size = 2574004;
+	char *filename = "/Users/rsaito/Desktop/TMP_Area/C_114STD_1.kiff";
+
+	unsigned char *uncompressbuf; // static char unc[10000000]; ??
+	uncompressbuf = (unsigned char *)malloc(uncompressed_size * sizeof(unsigned char));
+
+	read_uncompress_kiffchunk(
+			&filename,
+			&seek_pos, &compressed_size, &uncompressed_size,
+			uncompressbuf, &ret_val);
+
+	printf("Hello, world!\n");
+	printf("%d\n", ret_val);
+
+}
+
+
